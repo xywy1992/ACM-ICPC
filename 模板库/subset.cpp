ec431238f4cb7370993666532bcd92e75c2f9231
xywy1992 <xywy.1992@gmail.com>
2014-10-04 18:59:02 +0800

    Update subset.cpp

--- 模板库/subset.cpp
+++ 模板库/subset.cpp
@@ -1,1 +1,12 @@
-subset
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int s;
+	cin >> s;
+	for (int i = s; i; i = (i - 1) & s) {
+		cout << i << " " << (i ^ s) << endl;
+	}
+	return 0;
+}
